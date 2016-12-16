c3cca397457e693de5b6cf8882f786e9a170b109
PhuongHuyen <huyenngo23496@gmail.com>
2016-12-16 16:07:14 +0700

    Chinhsuafrmnhanvien

--- QuanLiQuanCOFFEE/frmNhanVien.cs
+++ QuanLiQuanCOFFEE/frmNhanVien.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace QuanLiQuanCOFFEE
 {
     public partial class FrmNhanVien : Form
@@ -15,6 +15,39 @@ namespace QuanLiQuanCOFFEE
         public FrmNhanVien()
         {
             InitializeComponent();
+        }
+        private void ketnoi()
+        {
+            SqlConnection kn = new SqlConnection(@"Data Source=HUYENNGO\SQLEXPRESS;Initial Catalog=qlBH;Integrated Security=True");
+            kn.Open();
+            string sql = "select *from NHANVIEN";
+            SqlCommand commandsql = new SqlCommand(sql, kn);
+            SqlDataAdapter com = new SqlDataAdapter(commandsql);
+            DataTable table = new DataTable();
+            com.Fill(table);
+            dgvNV.DataSource = table;
+
+        }
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmNhanVien_Load(object sender, EventArgs e)
+        {
+            ketnoi();
+
+
         }
     }
 }
